8a7b94c5fc7747be40342d7dd3c95d17094c3e3f
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-04 13:51:58 -0300

    feat (api): adicionar controller de Spent para as requisições

---
+++ backend/src/main/java/com/finquest/backend/controller/SpentController.java
@@ -0,0 +1,38 @@
+package com.finquest.backend.controller;
+
+import com.finquest.backend.dto.request.SpentRequestDTO;
+import com.finquest.backend.dto.response.SpentResponseDTO;
+import com.finquest.backend.mapper.SpentMapper;
+import com.finquest.backend.model.Spent;
+import com.finquest.backend.service.SpentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/spents")
+public class SpentController {
+
+    private final SpentService service;
+    private final SpentMapper mapper;
+
+    public SpentController(SpentService service,  SpentMapper mapper) {
+        this.service = service;
+        this.mapper = mapper;
+    }
+
+    @PostMapping
+    public ResponseEntity<SpentResponseDTO> save(@RequestBody SpentRequestDTO request) {
+        Spent spent = mapper.toEntity(request);
+        var entitySaved = service.save(spent);
+
+        return ResponseEntity.ok(mapper.toDto(entitySaved));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SpentResponseDTO>> findAll() {
+        List<Spent> spents = service.findAll();
+        return ResponseEntity.ok(mapper.toDto(spents));
+    }
+}
