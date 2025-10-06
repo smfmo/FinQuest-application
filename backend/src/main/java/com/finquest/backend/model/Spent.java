231cd23fe73320ca3d8e871d233d70222f374fc1
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-06 16:21:00 -0300

    refactor (model): Implementar @Getter e @Setter do Lombok para substituir getters e setter manuais

--- backend/src/main/java/com/finquest/backend/model/Spent.java
+++ backend/src/main/java/com/finquest/backend/model/Spent.java
@@ -2,15 +2,17 @@ package com.finquest.backend.model;
 
 import com.finquest.backend.model.enums.Category;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Table(name = "spent",
         schema = "public")
+@Getter @Setter
 @EntityListeners(AuditingEntityListener.class)
 public class Spent {
 
@@ -46,49 +48,6 @@ public class Spent {
         this.date = date;
         this.category = category;
         this.amountSpent = amountSpent;
-        this.user = user;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-    public void setId(UUID id) {
-        this.id = id;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public LocalDateTime getDate() {
-        return date;
-    }
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-    public Category getCategory() {
-        return category;
-    }
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-    public Double getAmountSpent() {
-        return amountSpent;
-    }
-    public void setAmountSpent(Double amountSpent) {
-        this.amountSpent = amountSpent;
-    }
-    public User getUser() {
-        return user;
-    }
-    public void setUser(User user) {
         this.user = user;
     }
 }
