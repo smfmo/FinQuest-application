e970436122f87bad50e962a1272c39124a9fc574
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-06 16:20:25 -0300

    refactor (model): Implemntar @Getter e @Setter do Lombok para substituir getters e setter manuais

--- backend/src/main/java/com/finquest/backend/model/User.java
+++ backend/src/main/java/com/finquest/backend/model/User.java
@@ -1,6 +1,8 @@
 package com.finquest.backend.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
@@ -10,6 +12,7 @@ import java.util.UUID;
 @Entity
 @Table(name = "app_user",
         schema = "public")
+@Getter @Setter
 @EntityListeners(AuditingEntityListener.class)
 public class User {
 
@@ -51,49 +54,6 @@ public class User {
         this.password = password;
         this.email = email;
         this.spents = spents;
-        this.createdDate = createdDate;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-    public void setId(UUID id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public List<Spent> getSpents() {
-        return spents;
-    }
-    public void setSpents(List<Spent> spents) {
-        this.spents = spents;
-    }
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
-    public void setCreatedDate(LocalDateTime createdDate) {
         this.createdDate = createdDate;
     }
 }
