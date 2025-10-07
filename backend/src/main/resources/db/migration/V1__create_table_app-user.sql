d0821b27986fbbd53004a9a35b83b6a09fcf5db8
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-07 16:26:30 -0300

    feat (persistence): Adicionar campo de auditoria (lastModifiedDate)

--- backend/src/main/resources/db/migration/V1__create_table_app-user.sql
+++ backend/src/main/resources/db/migration/V1__create_table_app-user.sql
@@ -4,5 +4,6 @@ CREATE TABLE app_user ( -- system user --
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    created_date TIMESTAMP
+    created_date TIMESTAMP,
+    last_modified_date TIMESTAMP
 );
