6ed30376efee3c3ce6da20de6f9ca2c43dd0cfd1
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-07 16:27:41 -0300

    feat (persistence): Adicionar campo de auditoria para data de ultima modificação e mudança no tipo do campo de amount_spent para NUMERIC(19, 2)

--- backend/src/main/resources/db/migration/V2__create_table_spent.sql
+++ backend/src/main/resources/db/migration/V2__create_table_spent.sql
@@ -4,9 +4,10 @@ CREATE TABLE spent (
     description TEXT,
     date TIMESTAMP,
     category VARCHAR(50) NOT NULL,
-    amount_spent DECIMAL(10,2) NOT NULL,
+    amount_spent NUMERIC(19, 2) NOT NULL,
     user_id uuid NOT NULL,
     created_date TIMESTAMP,
+    last_modified_date TIMESTAMP,
 
     CONSTRAINT fk_spent_app_user FOREIGN KEY (user_id) REFERENCES app_user(id) ON DELETE CASCADE,
     CONSTRAINT chk_category CHECK ( category in ('SAUDE', 'LAZER', 'ESTUDO', 'OUTROS') )
