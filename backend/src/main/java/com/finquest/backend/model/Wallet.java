f74d02e968965cc9f168a3a029ce595b889ea4a2
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-07 16:02:39 -0300

    feat (model): Implementar entidade Wallet (carteira)
    - adiciona entidade wallet que contém o campo de saldo, que representa a carteira de um usuário.
    - implementa relação one-to-one entre User e Wallet

---
+++ backend/src/main/java/com/finquest/backend/model/Wallet.java
@@ -0,0 +1,27 @@
+package com.finquest.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "wallet",
+        schema = "public")
+@Getter @Setter
+public class Wallet extends Audit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "balance")
+    private BigDecimal balance = BigDecimal.ZERO;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Wallet() {}
+}
