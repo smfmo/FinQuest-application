b29006767aa25ec05277d34bb4415456a7b4f60a
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-07 16:40:18 -0300

    feat (service): Adicionar métodos de buscar pelo Id da meta e atualização parcial de alguns campos necessários

--- backend/src/main/java/com/finquest/backend/service/IndividualGoalService.java
+++ backend/src/main/java/com/finquest/backend/service/IndividualGoalService.java
@@ -1,11 +1,13 @@
 package com.finquest.backend.service;
 
+import com.finquest.backend.dto.request.IndividualGoalRequestDTO;
 import com.finquest.backend.model.IndividualGoal;
 import com.finquest.backend.repository.IndividualGoalRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class IndividualGoalService {
@@ -23,5 +25,27 @@ public class IndividualGoalService {
 
     public List<IndividualGoal> findAll() {
         return repository.findAll();
+    }
+
+    public IndividualGoal getById(UUID id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Goal not found with id: " + id));
+    }
+
+    @Transactional
+    public IndividualGoal partialUpdate(UUID id, IndividualGoalRequestDTO updatedIndividualGoal) {
+        IndividualGoal existingGoal = getById(id);
+
+        if (updatedIndividualGoal.title() != null) {
+            existingGoal.setTitle(updatedIndividualGoal.title());
+        }
+        if (updatedIndividualGoal.description() != null) {
+            existingGoal.setDescription(updatedIndividualGoal.description());
+        }
+        if (updatedIndividualGoal.term() != null) {
+            existingGoal.setTerm(updatedIndividualGoal.term());
+        }
+
+        return repository.save(existingGoal);
     }
 }
