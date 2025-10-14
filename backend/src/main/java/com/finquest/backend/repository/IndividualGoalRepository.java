20e29b927b1e43567aa57075dcff9c327469684c
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-14 12:05:44 -0300

    feat (persistence): Adicionar método que busca o usuário proprietário das metas

--- backend/src/main/java/com/finquest/backend/repository/IndividualGoalRepository.java
+++ backend/src/main/java/com/finquest/backend/repository/IndividualGoalRepository.java
@@ -1,10 +1,14 @@
 package com.finquest.backend.repository;
 
 import com.finquest.backend.model.IndividualGoal;
+import com.finquest.backend.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface IndividualGoalRepository extends JpaRepository<IndividualGoal, UUID> {
+    List<IndividualGoal> findByUser(UserEntity user);
 }
