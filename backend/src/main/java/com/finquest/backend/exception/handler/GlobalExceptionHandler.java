5cc3ac0bf664d0b0cae55525d1cba07dad35f142
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-14 12:36:46 -0300

    feat (exception): Adicionar GlobalExceptionHandler para tratamento de exceções globais
    - Adiciona tratamento de excessão para os campos de validação do Validation
    - Adiciona tratamento para UserMismatchException que é uma excessão personalizada
    - Adiciona tratamento para WalletOwnershipException que é uma excessão personalizada

---
+++ backend/src/main/java/com/finquest/backend/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.finquest.backend.exception.handler;
+
+import com.finquest.backend.exception.custom.UserMismatchException;
+import com.finquest.backend.exception.custom.WalletOwnershipException;
+import com.finquest.backend.exception.response.ErrorResponseDTO;
+import com.finquest.backend.exception.response.FieldErrorResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ErrorResponseDTO handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        List<FieldError> fieldErrors = e.getFieldErrors();
+
+        List<FieldErrorResponseDTO> listErrors = fieldErrors
+                .stream()
+                .map(fe -> new FieldErrorResponseDTO(fe.getField(), fe.getDefaultMessage()))
+                .toList();
+
+        return new ErrorResponseDTO(HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                "Validation Error", listErrors);
+    }
+
+    @ExceptionHandler(UserMismatchException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorResponseDTO handlerUserMismatchException(UserMismatchException e) {
+        return new ErrorResponseDTO(HttpStatus.FORBIDDEN.value(), e.getMessage(), List.of());
+    }
+
+    @ExceptionHandler(WalletOwnershipException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorResponseDTO handleWalletOwnershipException(WalletOwnershipException e) {
+        return new ErrorResponseDTO(HttpStatus.FORBIDDEN.value(), e.getMessage(), List.of());
+    }
+}
