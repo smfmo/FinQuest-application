6594506700e13275ef7ed56231f5663f7b17df06
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-13 21:42:10 -0300

    feat (persistence): Adicionar método que busca o username do usuário no banco de dados

--- backend/src/main/java/com/finquest/backend/repository/UserEntityRepository.java
+++ backend/src/main/java/com/finquest/backend/repository/UserEntityRepository.java
@@ -4,9 +4,10 @@ import com.finquest.backend.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, UUID> {
-
+public interface UserEntityRepository extends JpaRepository<UserEntity, UUID> {
+    Optional<UserEntity> findByUsername(String username);
 }
