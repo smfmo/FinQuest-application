42ecd132c2a3152a1e945cb04b12ff78afb98d21
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-14 12:02:54 -0300

    feat (persistence): Adicionar método que busca o usuário proprietário das metas

--- backend/src/main/java/com/finquest/backend/service/IndividualGoalService.java
+++ backend/src/main/java/com/finquest/backend/service/IndividualGoalService.java
@@ -1,8 +1,12 @@
 package com.finquest.backend.service;
 
 import com.finquest.backend.dto.request.IndividualGoalRequestDTO;
+import com.finquest.backend.exception.custom.UserMismatchException;
 import com.finquest.backend.model.IndividualGoal;
+import com.finquest.backend.model.UserEntity;
 import com.finquest.backend.repository.IndividualGoalRepository;
+import com.finquest.backend.security.SecurityService;
+import com.finquest.backend.validator.UserOwnershipValidator;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,18 +17,25 @@ import java.util.UUID;
 public class IndividualGoalService {
 
     private final IndividualGoalRepository repository;
+    private final UserOwnershipValidator userOwnershipValidator;
+    private final SecurityService securityService;
 
-    public IndividualGoalService(IndividualGoalRepository repository) {
+    public IndividualGoalService(IndividualGoalRepository repository, UserOwnershipValidator userOwnershipValidator,
+                                 SecurityService securityService) {
         this.repository = repository;
+        this.userOwnershipValidator = userOwnershipValidator;
+        this.securityService = securityService;
     }
 
     @Transactional
     public IndividualGoal save(IndividualGoal goal) {
+        userOwnershipValidator.validateUserOwnerShip(goal);
         return repository.save(goal);
     }
 
-    public List<IndividualGoal> findAll() {
-        return repository.findAll();
+    public List<IndividualGoal> findAllByUserAuthenticated() {
+        UserEntity user = securityService.getUserAuthenticated();
+        return repository.findByUser(user);
     }
 
     public IndividualGoal getById(UUID id) {
