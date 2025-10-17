2e58473d6a953aa22fad8af6d183e4685b487a2e
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-17 18:21:55 -0300

    feat (service): Injetar SpentValidator para validar na hora de persistir um gasto no banco

--- backend/src/main/java/com/finquest/backend/service/SpentService.java
+++ backend/src/main/java/com/finquest/backend/service/SpentService.java
@@ -1,11 +1,11 @@
 package com.finquest.backend.service;
 
-import com.finquest.backend.exception.custom.UserMismatchException;
 import com.finquest.backend.model.Spent;
 import com.finquest.backend.model.UserEntity;
 import com.finquest.backend.model.Wallet;
 import com.finquest.backend.repository.SpentRepository;
 import com.finquest.backend.security.SecurityService;
+import com.finquest.backend.validator.SpentValidator;
 import com.finquest.backend.validator.UserOwnershipValidator;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,18 +17,21 @@ public class SpentService {
     private final SpentRepository repository;
     private final UserOwnershipValidator userOwnershipValidator;
     private final SecurityService securityService;
+    private final SpentValidator spentValidator;
 
 
     public SpentService(SpentRepository repository, UserOwnershipValidator userOwnershipValidator,
-                        SecurityService securityService) {
+                        SecurityService securityService, SpentValidator spentValidator) {
         this.repository = repository;
         this.userOwnershipValidator = userOwnershipValidator;
         this.securityService = securityService;
+        this.spentValidator = spentValidator;
     }
 
     @Transactional
     public Spent save(Spent spent) {
         userOwnershipValidator.validateUserOwnerShip(spent);
+        spentValidator.validate(spent);
         UserEntity user = spent.getUser();
         Wallet wallet = user.getWallet();
 
