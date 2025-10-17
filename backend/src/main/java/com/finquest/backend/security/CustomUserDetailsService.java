3f373a59f4d684b7d255814ee3d27e26f19c3833
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-17 18:16:06 -0300

    feat (security): Adicionar roles na hora de buildar um userDetails, continha apenas username e password

--- backend/src/main/java/com/finquest/backend/security/CustomUserDetailsService.java
+++ backend/src/main/java/com/finquest/backend/security/CustomUserDetailsService.java
@@ -27,6 +27,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         return User.builder()
                 .username(user.getUsername())
                 .password(user.getPassword())
+                .roles(String.valueOf(user.getRole()))
                 .build();
     }
 }
