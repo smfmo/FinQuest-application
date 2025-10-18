7a8d44d022d6760c11e536c6e4cd3e264480fbdd
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-17 21:05:48 -0300

    feat (dto): Implementar validações que faltavam no UserRequestDTO

--- backend/src/main/java/com/finquest/backend/dto/request/UserRequestDTO.java
+++ backend/src/main/java/com/finquest/backend/dto/request/UserRequestDTO.java
@@ -1,5 +1,6 @@
 package com.finquest.backend.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -7,7 +8,13 @@ public record UserRequestDTO(
         @NotBlank(message = "Nome obrigatório")
         @Size(min = 15, max = 255)
         String name,
+        @NotBlank(message = "Nome de usuário obrigatório")
+        @Size(min = 15, max = 255)
         String username,
+        @NotBlank(message = "Senha é obrigatória")
+        @Size(min = 15, max = 255)
         String password,
+        @NotBlank(message = "Email é obrigatório")
+        @Email(message = "Deve ser um email válido")
         String email
 ) {}
